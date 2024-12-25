5cd83c90024faa4791e5db6c9c6b62f57dbc5dbd
munimdev <munimzafar100@gmail.com>
2024-12-25 19:32:26 +0500

    install eslint & prettier

---
+++ eslint.config.mjs
@@ -0,0 +1,19 @@
+import globals from "globals"
+import pluginJs from "@eslint/js"
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+    {
+        files: ["**/*.js"],
+        languageOptions: { sourceType: "commonjs" },
+    },
+    {
+        languageOptions: { globals: { ...globals.browser, ...globals.node } },
+    },
+    {
+        rules: {
+            ...pluginJs.configs.recommended.rules,
+            "no-unused-vars": "off",
+        },
+    },
+]
